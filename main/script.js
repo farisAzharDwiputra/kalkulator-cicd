6219661de9ee4f047e49aa71796770dfeb509695
farisAzharDwiputra <farisAzharDwiputra@users.noreply.github.com>
2025-06-13 15:17:20 +0000

    deploy: 65fd1072a387cac0774cdf903bc6fc1b17887b24

--- main/script.js
+++ main/script.js
@@ -13,8 +13,7 @@ function isOperator(value) {
 }
 
 function clearDisplay() {
-    display.value = '';
-    let unusedVariableForDemo = "hello"; // Variabel tidak digunakan 
+  display.value = '';
 }
 
 function deleteLast() {
